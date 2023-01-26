a3c63eb1efd6dcb6bb0c261a392cdb59e3157792
Albert Pangestu <stamina.left@gmail.com>
2023-01-26 19:30:02 +0700

    Initial Commit

---
+++ iOS-Instruction/ViewControllers/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  iOS-Instruction
+//
+//  Created by Albert on 26/01/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
